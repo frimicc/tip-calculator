0e70f8084cdc6bc0ea1365708f74d88a116ce836
Michael Friedman <frimicc@gmail.com>
2017-01-12 18:42:49 -0800

    Initial Commit

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by  Michael Friedman on 1/12/17.
+//  Copyright © 2017  Michael Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by  Michael Friedman on 1/12/17.
+//  Copyright © 2017  Michael Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
